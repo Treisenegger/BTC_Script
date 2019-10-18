e99b96feedd758680eb6c2e87eab15cc66157b03
Thomas Reisenegger <thomas.reiseneggerb@gmail.com>
2019-10-18 20:00:47 -0300

    Incorporate conditional execution of instructions

--- btc_script.pl
+++ btc_script.pl
@@ -7,30 +7,46 @@ primitive_action(op_pick).
 primitive_action(op_if).
 
 poss(op_push(E), S).
-poss(op_dup, S) :- holds(stack(E, 0), S).
-poss(op_hash160, S) :- holds(stack(E, 0), S), holds(stack(E1, P), S), P1 is P + 1, not(holds(stack(E2, P1), S)), hash160(H, E1).
-poss(op_equalverify, S) :- holds(stack(E1, 0), S), holds(stack(E2, 1), S).
-poss(op_checksig, S) :- holds(stack(E1, 0), S), holds(stack(E2, 1), S).
-poss(op_pick, S) :- holds(stack(E, 0), S), holds(stack(E1, P), S), P1 is P + 1, not(holds(stack(E2, P1), S)), integer(E1), E1 >= 0, P > E1.
+poss(op_dup, S) :- holds(stack(E, 0), S) ;
+    holds(if_valid(VD, V), S), VD1 is VD + 1, not(holds(if_valid(VD1, V1), S)), V = 0 ;
+    holds(if_valid(VD, V), S), VD1 is VD + 1, not(holds(if_valid(VD1, V1), S)), V = 1, holds(if_stack(VD, CS), S), CS = 0.
+poss(op_hash160, S) :- holds(stack(E, 0), S), holds(stack(E1, P), S), P1 is P + 1, not(holds(stack(E2, P1), S)), hash160(H, E1) ;
+    holds(if_valid(VD, V), S), VD1 is VD + 1, not(holds(if_valid(VD1, V1), S)), V = 0 ;
+    holds(if_valid(VD, V), S), VD1 is VD + 1, not(holds(if_valid(VD1, V1), S)), V = 1, holds(if_stack(VD, CS), S), CS = 0.
+poss(op_equalverify, S) :- holds(stack(E1, 0), S), holds(stack(E2, 1), S) ;
+    holds(if_valid(VD, V), S), VD1 is VD + 1, not(holds(if_valid(VD1, V1), S)), V = 0 ;
+    holds(if_valid(VD, V), S), VD1 is VD + 1, not(holds(if_valid(VD1, V1), S)), V = 1, holds(if_stack(VD, CS), S), CS = 0.
+poss(op_checksig, S) :- holds(stack(E1, 0), S), holds(stack(E2, 1), S) ;
+    holds(if_valid(VD, V), S), VD1 is VD + 1, not(holds(if_valid(VD1, V1), S)), V = 0 ;
+    holds(if_valid(VD, V), S), VD1 is VD + 1, not(holds(if_valid(VD1, V1), S)), V = 1, holds(if_stack(VD, CS), S), CS = 0.
+poss(op_pick, S) :- holds(stack(E, 0), S), holds(stack(E1, P), S), P1 is P + 1, not(holds(stack(E2, P1), S)), integer(E1), E1 >= 0, P > E1 ;
+    holds(if_valid(VD, V), S), VD1 is VD + 1, not(holds(if_valid(VD1, V1), S)), V = 0 ;
+    holds(if_valid(VD, V), S), VD1 is VD + 1, not(holds(if_valid(VD1, V1), S)), V = 1, holds(if_stack(VD, CS), S), CS = 0.
 poss(op_if, S) :- holds(stack(E, 0), S) ;
     holds(if_valid(VD, V), S), VD1 is VD + 1, not(holds(if_valid(VD1, V1), S)), V = 0 ;
     holds(if_valid(VD, V), S), VD1 is VD + 1, not(holds(if_valid(VD1, V1), S)), V = 1, holds(if_stack(VD, CS), S), CS = 0.
 
 /* Element, position */
-holds(stack(E, P), do(A, S)) :- A = op_push(E), P = 0, not(holds(stack(E1, P), S)) ;
-    A = op_push(E), holds(stack(E2, P1), S), P is P1 + 1, not(holds(stack(E1, P), S)) ;
-    A = op_dup, holds(stack(E, P1), S), P is P1 + 1, not(holds(stack(E1, P), S)) ;
-    A = op_hash160, holds(stack(E1, P), S), P1 is P + 1, not(holds(stack(E2, P1), S)), hash160(E, E1) ;
-    A = op_checksig, holds(stack(E1, P), S), P1 is P + 1, P2 is P1 + 1, holds(stack(E2, P1), S), not(holds(stack(E3, P2), S)), sig(E1, E2), E = 1 ;
-    A = op_checksig, holds(stack(E1, P), S), P1 is P + 1, P2 is P1 + 1, holds(stack(E2, P1), S), not(holds(stack(E3, P2), S)), not(sig(E1, E2)), E = 0 ;
-    A = op_pick, holds(stack(E1, P), S), P1 is P + 1, not(holds(stack(E2, P1), S)), P2 is P - 1 - E1, holds(stack(E, P2), S) ;
-    holds(stack(E, P), S), not((
-        A = op_hash160, P1 is P + 1, not(holds(stack(E1, P1), S)) ;
-        A = op_equalverify, P1 is P + 2, not(holds(stack(E1, P1), S)) ;
-        A = op_checksig, P1 is P + 2, not(holds(stack(E1, P1), S)) ;
-        A = op_pick, P1 is P + 1, not(holds(stack(E1, P1), S)) ;
-        A = op_if, P1 is P + 1, not(holds(stack(E1, P1), S))
-    )).
+holds(stack(E, P), do(A, S)) :- (not(holds(if_valid(0, V), S)) ; holds(if_valid(VD, V), S), VD1 is VD + 1, not(holds(if_valid(VD1, V1), S)), V = 1, holds(if_stack(VD, CS), S), CS = 1), (
+        A = op_push(E), P = 0, not(holds(stack(E1, P), S)) ;
+        A = op_push(E), holds(stack(E2, P1), S), P is P1 + 1, not(holds(stack(E1, P), S)) ;
+        A = op_dup, holds(stack(E, P1), S), P is P1 + 1, not(holds(stack(E1, P), S)) ;
+        A = op_hash160, holds(stack(E1, P), S), P1 is P + 1, not(holds(stack(E2, P1), S)), hash160(E, E1) ;
+        A = op_checksig, holds(stack(E1, P), S), P1 is P + 1, P2 is P1 + 1, holds(stack(E2, P1), S), not(holds(stack(E3, P2), S)), sig(E1, E2), E = 1 ;
+        A = op_checksig, holds(stack(E1, P), S), P1 is P + 1, P2 is P1 + 1, holds(stack(E2, P1), S), not(holds(stack(E3, P2), S)), not(sig(E1, E2)), E = 0 ;
+        A = op_pick, holds(stack(E1, P), S), P1 is P + 1, not(holds(stack(E2, P1), S)), P2 is P - 1 - E1, holds(stack(E, P2), S) ;
+        holds(stack(E, P), S), not((
+            A = op_hash160, P1 is P + 1, not(holds(stack(E1, P1), S)) ;
+            A = op_equalverify, P1 is P + 2, not(holds(stack(E1, P1), S)) ;
+            A = op_checksig, P1 is P + 2, not(holds(stack(E1, P1), S)) ;
+            A = op_pick, P1 is P + 1, not(holds(stack(E1, P1), S)) ;
+            A = op_if, P1 is P + 1, not(holds(stack(E1, P1), S))
+        ))
+    ) ;
+    ((
+        holds(if_valid(VD, V), S), VD1 is VD + 1, not(holds(if_valid(VD1, V1), S)), V = 0 ;
+        holds(if_valid(VD, V), S), VD1 is VD + 1, not(holds(if_valid(VD1, V1), S)), V = 1, holds(if_stack(VD, CS), S), CS = 0
+    ), holds(stack(E, P), S)).
 
 holds(error, do(A, S)) :- A = op_equalverify, holds(stack(E1, P), S), P1 is P + 1, P2 is P1 + 1, holds(stack(E2, P1), S), not(holds(stack(E3, P2), S)), not(E1 = E2) ;
     holds(error, S).
@@ -40,7 +56,7 @@ holds(if_stack(D, CS), do(A, S)) :- A = op_if, D = 0, not(holds(if_stack(D, CS1)
         E \= 0, CS = 1 ;
         E = 0, CS = 0
     ) ;
-    A = op_if, V = 1, holds(if_valid(VD, V), S), D is VD + 1, not(holds(if_valid(D, V1), S)), holds(if_stack(VD, CS1), S), CS1 = 1, holds(stack(E, P), S), P1 is P + 1, not(holds(stack(E1, P1), S)), (
+    A = op_if, V = 1, holds(if_valid(VD, V), S), D is VD + 1, not(holds(if_valid(D, V1), S)), CS1 = 1, holds(if_stack(VD, CS1), S), holds(stack(E, P), S), P1 is P + 1, not(holds(stack(E1, P1), S)), (
         E \= 0, CS = 1 ;
         E = 0, CS = 0
     ) ;
