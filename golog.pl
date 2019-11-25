b035c1b1f6c054acad541e15877ca9628ddd7cd7
Thomas Reisenegger <thomas.reiseneggerb@gmail.com>
2019-11-25 19:51:04 -0300

    Use anonymous variables in golog script

--- golog.pl
+++ golog.pl
@@ -10,10 +10,10 @@ do(pi(V,E),S,S1) :- sub(V,_,E,E1), do(E1,S,S1).
 /* do(E,S,S1) :- proc(E,E1), do(E1,S,S1). */
 do(E,S,do(E,S)) :- primitive_action(E), poss(E,S).
 
-sub(X1,X2,T1,T2) :- var(T1), T2 = T1.
+sub(_,_,T1,T2) :- var(T1), T2 = T1.
 sub(X1,X2,T1,T2) :- not(var(T1)), T1 = X1, T2 = X2.
 sub(X1,X2,T1,T2) :- not(T1 = X1), T1 =..[F|L1], sub_list(X1,X2,L1,L2), T2 =..[F|L2].
-sub_list(X1,X2,[],[]).
+sub_list(_,_,[],[]).
 sub_list(X1,X2,[T1|L1],[T2|L2]) :- sub(X1,X2,T1,T2), sub_list(X1,X2,L1,L2).
 
 holds(and(P1,P2),S) :- holds(P1,S), holds(P2,S).
